28bb5042abca2ac8437bad40011e2b4dcf5358b7
kameni4400 <kriskaidan@gmail.com>
2022-04-08 14:54:56 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*	Description: main - print single digit number,
+*	Return: 0 if success.
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 48 ; num <= 57 ; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}	
