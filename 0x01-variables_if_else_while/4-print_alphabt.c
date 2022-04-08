73b5d3d273ca5068568a0d91a0bb4ecf471f51e0
kameni4400 <kriskaidan@gmail.com>
2022-04-08 14:30:22 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*	Description: main - prints out alphabt in lowercase,
+*	Return: 0 if success.
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+
+		if (alpha != 'q' && alpha != 'e')
+
+			putchar(alpha);
+	putchar('\n');
+	return (0);
+}	
