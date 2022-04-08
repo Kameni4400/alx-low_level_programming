51f69b734fb41a6a460301a0d08bb46386db629a
kameni4400 <kriskaidan@gmail.com>
2022-04-08 15:21:45 -0700

    Base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*	Description: main - prints all numbers of base,
+*	Return: 0 if success.
+*/
+int main(void)
+{
+	int numb;
+	char alphab;
+
+	for (numb = 48 ; numb <= 57 ; numb++)
+		putchar(numb);
+	for (alphab = 97 ; alphab <= 102 ; alphab++)
+		putchar(alphab);
+	putchar('\n');
+	return (0);
+}
+		
