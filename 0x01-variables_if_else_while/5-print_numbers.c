62555b33870c048512f35d86accc5e6f7a1d3769
kameni4400 <kriskaidan@gmail.com>
2022-04-08 14:40:03 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*	Description: main - print all digit number of base,
+*	Return: 0 if success.
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9 ; num++)
+		printf("%d", num);
+	puts("");
+	return (0);
+}
