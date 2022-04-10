6db3667497cd1acc8fed44d5abd8899e0e870ee5
kameni4400 <kriskaidan@gmail.com>
2022-04-10 06:36:57 -0700

    Quot

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
 *	Description: main - prints out Dora's quote,
-*	Return: 1 if success.
+*	Return: 0 if success.
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, 59);
 	return (1);
-}	
+}
