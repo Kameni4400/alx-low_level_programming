339c846d0bdc1b4c04dee19aff47294ec299f819
kameni4400 <kriskaidan@gmail.com>
2022-04-10 05:59:18 -0700

    Com

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,17 @@ int main(void)
 {
 	int numb;
 
-	for (numb = '0' ; numb <= '9' ; numb++)
+	for (numb = 0 ; numb <= 9 ; numb++)
+	{
+		putchar((numb % 10) + '0');
 
-		putchar(numb);
-
-		if (numb != '9')
+		if (numb == 9)
+			continue;
 
 
 			putchar(',');
 			putchar(' ');
-
+	}
 	putchar('\n');
 	return (0);
 }
