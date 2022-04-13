89fa160c62ad8bee582e3032d8513b30c5b5c367
kameni4400 <kriskaidan@gmail.com>
2022-04-13 05:18:52 -0700

    Alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*	Description: main - print the alphabet in lowercase,
+*	Return: 0 if success.
+*/
+void print_alphabet(void)
+{
+	int alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}	
