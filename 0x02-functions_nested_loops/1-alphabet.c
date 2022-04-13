d788733448623ec203b85562a243a71e835cd783
kameni4400 <kriskaidan@gmail.com>
2022-04-13 05:35:41 -0700

    spac

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*	print_alphabet: prints the alphabet in lowercase,
+*	Description: prints the alphabet in lowercase, with new line,
 *	Return: 0 if success.
 */
 void print_alphabet(void)
